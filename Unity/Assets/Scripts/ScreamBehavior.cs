cfad0f582f979ae57a22215908ebb33d52167b9d
rodrigomp6 <rodrigomp6@gmail.com>
2021-03-25 20:07:50 -0300

    Montagem de cena

---
+++ Unity/Assets/Scripts/ScreamBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class ScreamBehavior : MonoBehaviour
+{
+    AudioSource audioSource;
+    public Camera cameraTeste;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = cameraTeste.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.name == "Panel (1)") {
+                    audioSource.Play();
+                };
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Ray ray = cameraTeste.ScreenPointToRay(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            Debug.DrawLine(ray.origin, hit.point);
+    }
+
+}
+
