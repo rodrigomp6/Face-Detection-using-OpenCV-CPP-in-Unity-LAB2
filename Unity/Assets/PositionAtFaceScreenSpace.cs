2e33f2a04591b1ac80be4de076931d7b20c9d867
rodrigomp6 <rodrigomp6@gmail.com>
2021-04-07 15:20:18 -0300

    Estabilização da câmera e inclusão do mostro

--- Unity/Assets/PositionAtFaceScreenSpace.cs
+++ Unity/Assets/PositionAtFaceScreenSpace.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PositionAtFaceScreenSpace : MonoBehaviour
 {
-    //private float _camDistance;
+    private float _camDistance;
     private readonly float[] distances = new float[600];
     private int distancesArrayPos = 0;
     private float averageDistance = 0;
@@ -14,16 +14,22 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //_camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+/*        var personagem = GameObject.Find("Personagem");
+        if (personagem == null)
+            return;
+        _camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+*/
+
+
     }
 
     void Update()
     {
         if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
             return;
-        Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].z*-1);
-        transform.position = Vector3.Lerp(transform.position, headpos, Time.deltaTime);
-        calculateScare(headpos);
+                Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y);
+                transform.position = Vector3.Lerp(transform.position, headpos, Time.deltaTime);
+                calculateScare(headpos);
     }
 
     private void calculateScare(Vector3 headPosition)
@@ -31,14 +37,17 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
         var distance = Vector3.Distance(lastHeadPos, headPosition);
         lastHeadPos = transform.position;
         float variation = Math.Abs(distance * averageDistance);
-        if (variation >= 0.5)
+        if (variation >= .6)
         {
             if (!audioSource.isPlaying)
                 audioSource.Play();
         }
         distances[distancesArrayPos] = distance;
         if (distancesArrayPos >= 599)
+        {
+            Debug.Log("Pronto para calcular susto");
             distancesArrayPos = 0;
+        }
         else
             distancesArrayPos++;
         averageDistance = getAverageDistance();
